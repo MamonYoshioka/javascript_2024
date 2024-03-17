aa4cd140a651a0735d02a83207b3cc53f966ced2
Mon <yoshiokamamon@gmail.com>
2024-03-17 15:18:41 +0900

    UPDATE Files

--- js/if.js
+++ js/if.js
@@ -4,7 +4,15 @@
 */
 
 let num = 100;
+num = 1;
+num = 0;
+
+console.log(num >= 100);
 
 if (num >= 100) {
     alert("numは100以上の数です。");
+}else if(num === 0){
+    document.write("<div class='zero'>numは0です!!</div>");
+}else{
+    document.write("<div class='sample'>numは100未満です</div>");
 }
