7c6ca2cb55f6ab82a47f7cd43c705e57d4df9eca
Mon <yoshiokamamon@gmail.com>
2024-03-17 14:31:38 +0900

    UPDATE Files

--- js/example.js
+++ js/example.js
@@ -1,1 +1,6 @@
 document.write("<p class='example'>この行はJavascripで書かれています</p>");
+
+
+let s = "Hello"
+s += "Web"
+document.write("<div class='example_2'>" + s +"</div>");
---
+++ js/sample1-2.js
@@ -0,0 +1,10 @@
+let num_1 = 5;
+num_1 += 2;
+
+let num_2 = 10;
+num_2 /= 2;
+// num_2 %= 2;
+
+document.write("<div class='sample'> 5 + 2 = " + num_1 + "</div>");
+document.write("<div class='sample'>10 / 2 = " + num_2 + "</div>");
+// document.write("10 % 2 =\t" + num_2 + "\n");
