cc4b607802f3c40341fb97152d17484a96e078f5
Mon <yoshiokamamon@gmail.com>
2024-03-31 14:53:24 +0900

    add "静的メソッドとプロパティ"

---
+++ js/obj6.js
@@ -0,0 +1,30 @@
+class Car {
+    // 生産台数
+    static number = 0;
+    constructor(){
+        // 生産台数を増やす
+        Car.number++;
+        // 製造番号を増やす
+        this.serial = Car.number;
+    }
+    // 製造番号の取得
+    getSerial(){
+        return this.serial;
+    }
+    // 生産台数の取得
+    static getNumber(){
+        return "自動車の生産台数：" + Car.number;
+    }
+}
+// 生産台数の表示確認
+document.write("<p>"+ Car.getNumber() +"</p>");
+
+// 自動車クラスのインスタンス生成
+car1 = new Car();
+car2 = new Car();
+
+document.write("<p>car1のシリアル番号:&nbsp"+ car1.getSerial() +"</p>");
+document.write("<p>car2のシリアル番号:&nbsp"+ car2.getSerial() +"</p>");
+
+// インスタンスを生成後の生産台数の表示確認
+document.write("<p>"+ Car.getNumber() +"</p>");
