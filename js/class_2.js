020b04c6de3be603dc828bd2a2a4138b05d07a7f
Mamon Yoshioka <yoshiokamamon@gmail.com>
2024-04-27 23:40:08 +0900

    add Students class

--- js/class_2.js
+++ js/class_2.js
@@ -4,15 +4,32 @@ class Person {
         this.age = age;
     }
     information(){ 
-        return `Name: ${this.name}, Age:${this.age}.`
+        return `Name: ${this.name}. Age:${this.age}.`
     } 
 }
 
+class Students extends Person {
+    constructor(name, age , grade){
+        super(name, age);
+        this.grade = grade;
+    }
+    information(){
+        return super.information() + `Grade:${this.grade}`
+    }
+
+}
 const person = new Person('太郎', 25);
 let info = person.information();
+
+const student = new Students('二郎', 20, 1);
+let st_info = student.information();
 
 const infoElement = document.createElement('div')
 infoElement.textContent = info;
 
 document.body.appendChild(infoElement);
 
+const studentElement = document.createElement('div')
+studentElement.textContent = st_info;
+
+document.body.appendChild(studentElement);
